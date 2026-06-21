32a8ae0c47f504b55b5a44c4576a3480e2db3e31
NITISH084 <imnitishkumar04@gmail.com>
2026-06-22 03:38:25 +0530

    fixed lint

--- core/storage/general_feedback/gae_models.py
+++ core/storage/general_feedback/gae_models.py
@@ -580,8 +580,8 @@ class PlatformFeedbackModel(BaseFeedbackModel):
                 return DESTINATION_TECHNICAL_CORE_TEAM
 
     @classmethod
-    def has_reference_to_user_id(cls, user_id: str) -> bool:
-        "Returns whether this model has a reference to the given user ID."
+    def has_reference_to_user_id(cls) -> bool:
+        """Returns whether t adhis model has a reference to the given user ID."""
         return False
 
     @classmethod
